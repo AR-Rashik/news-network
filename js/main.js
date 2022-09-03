0b2a8b8fe457d9043952275dac14bfa6eb9cc6c9
AR-Rashik <66801782+AR-Rashik@users.noreply.github.com>
2022-09-03 14:56:59 +0600

    Daynamic news categories added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-console.log('Hello');
+const loadAllNewsCategory = async() => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  
+  try{
+    const response = await fetch(url);
+    const data = await response.json();
+    displayAllNewsCategory(data.data.news_category);
+  }
+  catch (error){
+    console.log(error);
+  }
+}
+
+const displayAllNewsCategory = newsCategory => {
+  // console.log(category);
+
+  const allNewsCategory = document.getElementById('all-news-category');
+
+  for(let news of newsCategory){
+    // console.log(news);
+    const newsDiv = document.createElement('div');
+    newsDiv.innerHTML = `
+      <a class="text-decoration-none text-secondary px-3 mb-3" href="#"
+      >${news.category_name}</a>
+    `
+
+    allNewsCategory.appendChild(newsDiv);
+  }
+}
+
+loadAllNewsCategory();
